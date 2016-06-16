385a539ca7fcac8e5ffea91214938bb1c469e45b
NicoSD <n.berberich@gmail.com>
2016-06-16 23:51:18 +0200

    core pipeline finished, but only predicts 1's

---
+++ Write_Xtrain_to_csv.py
@@ -0,0 +1,83 @@
+__author__ = 'Nicolas'
+import numpy as np
+import time
+from sklearn import svm, grid_search
+import cPickle
+from random import *
+import glob
+import csv
+
+
+list_of_train_files = glob.glob('train/Data_*.csv')
+print('list_of_train_files: ', list_of_train_files[0:5])
+
+X_train=[]
+file_it=1
+
+for files in list_of_train_files:
+    data = np.zeros((300000,56))
+    EOG = np.zeros(300000)
+    feedback = np.zeros(300000)
+    with open(files, 'rb') as csvfile:
+         trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+         rownum = 0
+         for row in trial_reader:
+             #print(row[0])
+             if rownum>0:
+                colnum=0
+                for col in row:
+                    if colnum>0 and colnum<57 : # cut of the time column
+                        data[rownum-1][colnum-1]=row[colnum]
+                    if colnum == 57:
+                        EOG[rownum-1]=row[colnum]
+                    if colnum == 58:
+                        feedback[rownum-1]=row[colnum]
+                    colnum += 1
+             rownum += 1
+
+
+    data_shape =  np.shape(data)
+    print('Size of data: ',data_shape)
+
+    #print(data[0:1,0:1])
+    print('EOG: ', EOG[0:5])
+    print('feedback: ', feedback[0:5])
+    #print('Size of reshaped data_vector: ', np.shape(data_vector))
+
+    events = np.where(feedback == 1)
+    events = np.reshape(events,(-1,1))
+    print('events: ' ,events[0:2], np.shape(events))
+
+
+    X_new_file = []
+    it = 1
+    for feedback_event in events:
+        feature_vector = data[feedback_event : feedback_event+260, 39]  # channel CP2 is number 40
+        np.transpose(feature_vector)
+        if it==1:
+            X_new_file = feature_vector
+            #print(feature_vector, np.shape(feature_vector))
+        else:
+            X_new_file= np.vstack((X_new_file,feature_vector))
+        it+=1
+    #print('data X: ',X_new_file[0:3,0:5], np.shape(X_new_file), it)
+
+    if file_it == 1:
+        X_train = X_new_file
+    else:
+        X_train = np.vstack((X_train,X_new_file))
+    file_it+=1
+    print('shape of X_train: ', np.shape(X_train))
+
+############### Now write X_test to a csv file ###################
+'''
+with open('X_train.csv','wb') as csvfile:
+    result_writer = csv.writer(csvfile,delimiter=',')
+
+    X_shape = np.shape(X_train)         # 3400 x 260
+    for i in xrange(X_shape[0]):
+        for j in xrange(X_shape[1]) :
+            #result_writer.writerow([output[i],pred[i-1]])
+            result_writer.write([X_test[i,j]])
+'''
+np.savetxt('X_train.csv', X_train, delimiter=",")
---
+++ random_forest_classifier.py
@@ -0,0 +1,89 @@
+__author__ = 'Nicolas'
+
+
+import gzip
+import numpy as np
+import time
+from sklearn.ensemble import RandomForestClassifier
+import cPickle
+from random import *
+import glob
+import csv
+
+
+################ read in training labels ####################################
+labels = np.zeros(5440,dtype=object)
+with open('TrainLabels.csv', 'rb') as csvfile:
+     trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     rownum = 0
+     for row in trial_reader:
+         #print(row[0])
+         if rownum>0:
+            labels[rownum-1]=row[1]
+         rownum += 1
+
+print('Labels: ',labels[0:10])
+y = labels
+
+'''
+##################################### Read in train feature matrix X_train ##################################
+'''
+X_train = np.zeros((5440,260))
+with open('X_train.csv', 'rb') as csvfile:
+         trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+         rownum = 0
+         for row in trial_reader:
+             #print(row[0])
+             if rownum>0:
+                colnum=0
+                for col in row:
+                    X_train[rownum-1][colnum-1]=row[colnum]
+'''
+##################################### Read in test feature matrix X_train ##################################
+'''
+X_test = np.zeros((3400,260))
+with open('X_train.csv', 'rb') as csvfile:
+         trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+         rownum = 0
+         for row in trial_reader:
+             #print(row[0])
+             if rownum>0:
+                colnum=0
+                for col in row:
+                    X_test[rownum-1][colnum-1]=row[colnum]
+
+'''
+#### Now train a simple SVM classifier ####
+'''
+print('X_train: ', np.shape(X_train))
+print('X_test: ', np.shape(X_test))
+
+clf = RandomForestClassifier(n_estimators=10)
+clf.fit(X_train,y)
+
+
+pred = clf.predict(X_test)
+
+print(pred[0:10])
+
+
+output = np.zeros(3401,dtype=object)
+with open('SampleSubmission.csv', 'rb') as csvfile:
+     trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     rownum = 0
+     for row in trial_reader:
+         #print(row[0])
+         if rownum>0:
+            output[rownum]=row[0]
+         rownum += 1
+
+print(output[0:10])
+
+
+# now open the new submission file and write in the labels and the data
+with open('predictions.csv','wb') as csvfile:
+    result_writer = csv.writer(csvfile,delimiter=',')
+    result_writer.writerow(['IdFeedBack','Prediction'])
+
+    for i in range(1,3401):
+        result_writer.writerow([output[i],pred[i-1]])
