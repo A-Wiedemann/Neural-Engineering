501f947e895a709a36cb6dcee5b03cbf44218e8e
Andi <andreas.wiedemann@mail.de>
2016-07-02 00:51:02 +0200

    Extract for events sub matrix, convert to wave values, write Data.mat
    EEGLearn library supports a special format of the input data.
    For all csv files and events 1.3 secs of data for all 56 channels are
    extracted, are converted to average wave data in
    the form [theta_1, ..., theta_56, alpha_1, ..., beta_1, ..., beta_56]
    using EEGrunt and concatenated in the file Data.mat as variable featMat.
    Also trainlabels are added to this file.

--- EEGLearn/WriteDataMat.py
+++ EEGLearn/WriteDataMat.py
@@ -1,114 +1,68 @@
 import EEGrunt
+import glob
+import csv
+import numpy as np
+import scipy.io as sio
 
-source = 'openbci'
+source = 'bci-challenge'
 
-path = '../train/'
+list_of_train_files = glob.glob('../train/Data_*.csv')
 
-# EEG data file name
-filename = 'Data_S02_Sess01.csv'
+X_train=[]
+file_it=1
 
-# Session title (used in plots and such)
-session_title = "Data_S02_Sess01"
+for csvfile in list_of_train_files:
 
-# Initialize
-EEG = EEGrunt.EEGrunt(path, filename, source, session_title)
+    print('Loading csvfile: ',csvfile)
+    EOG = np.loadtxt(csvfile, delimiter=',',skiprows=1,usecols=[57])
+    feedback = np.loadtxt(csvfile, delimiter=',',skiprows=1,usecols=[58])
+    data = np.loadtxt(csvfile, delimiter=',',skiprows=1,usecols=range(0,57))
 
+    events = np.where(feedback == 1)
+    events = np.reshape(events,(-1,1))
 
-# Here we can set some additional properties
-# The 'plot' property determines whether plots are displayed or saved.
-# Possible values are 'show' and 'save'
-EEG.plot = 'save'
+    X_new_file = []
+    it = 1
 
-EEG.load_data()
+    for feedback_event in events:
+        feature_matrix = data[feedback_event : feedback_event+260, :]
+        EEG = EEGrunt.EEGrunt("", "", source, "")
+        EEG.set_data(feature_matrix)
+        # EEG.plot = 'show'
+        feature_vector = []
 
-# # print(EEG.channels)
-# EEG.load_channel(1)
-# print(EEG.bandpass(8,12))
-# EEG.remove_dc_offset()
-# EEG.notch_mains_interference()
-# EEG.signalplot()
-# EEG.get_spectrum_data()
-# EEG.spectrogram()
-# EEG.plot_band_power(8,12,"Alpha")
-# EEG.plot_spectrum_avg_fft()
+        for channel in EEG.channels:
+            EEG.load_channel(channel)
+            theta = np.mean(EEG.bandpass(4,8))
+            feature_vector.append(theta)
 
+        for channel in EEG.channels:
+            EEG.load_channel(channel)
+            alpha = np.mean(EEG.bandpass(8,12))
+            feature_vector.append(alpha)
 
+        for channel in EEG.channels:
+            EEG.load_channel(channel)
+            beta = np.mean(EEG.bandpass(13,30))
+            feature_vector.append(beta)
 
-# list_of_train_files = glob.glob('../train/Data_*.csv')
-# print('list_of_train_files: ', list_of_train_files[0:5])
-# list_of_train_files = ['../train/Data_S02_Sess01.csv']
+        if it==1:
+            X_new_file = feature_vector
+            #print(feature_vector, np.shape(feature_vector))
+        else:
+            X_new_file= np.vstack((X_new_file,feature_vector))
+        it+=1
+    #print('data X: ',X_new_file[0:3,0:5], np.shape(X_new_file), it)
 
-# X_train=[]
-# file_it=1
+    if file_it == 1:
+        X_train = X_new_file
+    else:
+        X_train = np.vstack((X_train,X_new_file))
+    file_it+=1
+    print('shape of X_train: ', np.shape(X_train))
 
-# for files in list_of_train_files:
-#     data = np.zeros((300000,56))
-#     EOG = np.zeros(300000)
-#     feedback = np.zeros(300000)
-#     with open(files, 'rb') as csvfile:
-#          trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#          rownum = 0
-#          for row in trial_reader:
-#              #print(row[0])
-#              if rownum>0:
-#                 colnum=0
-#                 for col in row:
-#                     if colnum>0 and colnum<57 : # cut of the time column
-#                         data[rownum-1][colnum-1]=row[colnum]
-#                     if colnum == 57:
-#                         EOG[rownum-1]=row[colnum]
-#                     if colnum == 58:
-#                         feedback[rownum-1]=row[colnum]
-#                     colnum += 1
-#              rownum += 1
+############### Now write X_test to a mat file ###################
+labels = np.loadtxt('../TrainLabels.csv', delimiter=',',skiprows=1,usecols=[1])
 
-
-#     data_shape =  np.shape(data)
-#     print('Size of data: ',data_shape)
-
-#     #print(data[0:1,0:1])
-#     print('EOG: ', EOG[0:5])
-#     print('feedback: ', feedback[0:5])
-#     #print('Size of reshaped data_vector: ', np.shape(data_vector))
-
-#     events = np.where(feedback == 1)
-#     events = np.reshape(events,(-1,1))
-#     print('events: ' ,events[0:2], np.shape(events))
-
-
-#     X_new_file = []
-#     it = 1
-#     for feedback_event in events:
-#         feature_matrix = data[feedback_event : feedback_event+260, :]
-        
-#         # feature_matrix = np.transpose(feature_matrix)
-#         # feature_vector = feature_matrix.ravel()
-#         # feature_vector = data[feedback_event : feedback_event+260, 29]  # channel CP2 is number 40, Cz is 30
-#         # np.transpose(feature_vector)
-#         if it==1:
-#             X_new_file = feature_vector
-#             #print(feature_vector, np.shape(feature_vector))
-#         else:
-#             X_new_file= np.vstack((X_new_file,feature_vector))
-#         it+=1
-#     #print('data X: ',X_new_file[0:3,0:5], np.shape(X_new_file), it)
-
-#     if file_it == 1:
-#         X_train = X_new_file
-#     else:
-#         X_train = np.vstack((X_train,X_new_file))
-#     file_it+=1
-#     print('shape of X_train: ', np.shape(X_train))
-
-# ############### Now write X_test to a csv file ###################
-# '''
-# with open('X_train.csv','wb') as csvfile:
-#     result_writer = csv.writer(csvfile,delimiter=',')
-
-#     X_shape = np.shape(X_train)         # 3400 x 260
-#     for i in xrange(X_shape[0]):
-#         for j in xrange(X_shape[1]) :
-#             #result_writer.writerow([output[i],pred[i-1]])
-#             result_writer.write([X_test[i,j]])
-# '''
 # np.savetxt('X_train.csv', X_train, delimiter=",")
+sio.savemat('Data.mat',{'featMat': X_train, 'labels': labels})
