f3ca5ea66077e1a9ff3076b06bf420f27694e200
Andreas Wiedemann <wiedeman@atcremers60.informatik.tu-muenchen.de>
2016-07-02 16:16:31 +0200

    Build up neural network using EEGLearn

--- EEGLearn/training.py
+++ EEGLearn/training.py
@@ -4,8 +4,10 @@ import numpy as np
 import csv
 import eeg_cnn_lib
 import utils
-import glob
 
+import theano
+import theano.tensor as T 
+import lasagne
 
 # import scipy.io as sio
 
@@ -21,92 +23,55 @@ with open("../Other/ChannelsLocation.csv","r") as csvfile:
     rownum = 0
     for row in reader:
         # print(row['Radius'],row['Phi'])
-        print(utils.pol2cart(float(row['Radius']),float(row['Phi'])))
+        # print(utils.pol2cart(float(row['Radius']),float(row['Phi'])))
         channelsLocation[rownum] = utils.pol2cart(float(row['Radius']),float(row['Phi']))
         rownum += 1
+
+print("Channel locations loaded.")
 ############################################################################################
 
 
+############ Setting up neural network #####################################################
+X_train, y_train = utils.load_data("Data.mat") 
+images = eeg_cnn_lib.gen_images(channelsLocation, X_train, 16, augment=True, pca=True, n_components=2)
 
-############ Read and convert training files ###############################################
-# 
-list_of_train_files = glob.glob('../train/Data_*.csv')
-print('list_of_train_files: ', list_of_train_files[0:5])
+# print(images)
+    # :return:            Tensor of size [samples, colors, W, H] containing generated
+    #                     images.
+# def build_cnn(input_var=None, W_init=None, n_layers=(4, 2, 1), n_filters_first=32, imSize=32, n_colors=3): 
+    # currently: np.shape(images) = (5440, 3, 16, 16)
+# input_var = T.TensorType('floatX', ((False,) * 5))()        # Notice the () at the end
+target_var = T.ivector('targets')
 
-X_train=[]
-file_it=1
-
-for files in list_of_train_files:
-    data = np.zeros((300000,56))
-    EOG = np.zeros(300000)
-    feedback = np.zeros(300000)
-    with open(files, 'rb') as csvfile:
-         trial_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-         rownum = 0
-         for row in trial_reader:
-             #print(row[0])
-             if rownum>0:
-                colnum=0
-                for col in row:
-                    if colnum>0 and colnum<57 : # cut of the time column
-                        data[rownum-1][colnum-1]=row[colnum]
-                    if colnum == 57:
-                        EOG[rownum-1]=row[colnum]
-                    if colnum == 58:
-                        feedback[rownum-1]=row[colnum]
-                    colnum += 1
-             rownum += 1
+input_var = T.tensor4('inputs')
+# target_var = T.ivector('targets')
 
 
-    data_shape =  np.shape(data)
-    print('Size of data: ',data_shape)
+network = eeg_cnn_lib.build_cnn(input_var=input_var, imSize=np.shape(images)[2], n_colors=np.shape(images)[1])
+############################################################################################
 
-    #print(data[0:1,0:1])
-    print('EOG: ', EOG[0:5])
-    print('feedback: ', feedback[0:5])
-    #print('Size of reshaped data_vector: ', np.shape(data_vector))
-
-    events = np.where(feedback == 1)
-    events = np.reshape(events,(-1,1))
-    print('events: ' ,events[0:2], np.shape(events))
+############ Training the thing ############################################################
 
 
-    X_new_file = []
-    it = 1
-    for feedback_event in events:
-        feature_matrix = data[feedback_event : feedback_event+260, :]
-        feature_matrix = np.transpose(feature_matrix)
-        feature_vector = feature_matrix.ravel()
-        # feature_vector = data[feedback_event : feedback_event+260, 29]  # channel CP2 is number 40, Cz is 30
-        # np.transpose(feature_vector)
-        if it==1:
-            X_new_file = feature_vector
-            #print(feature_vector, np.shape(feature_vector))
-        else:
-            X_new_file= np.vstack((X_new_file,feature_vector))
-        it+=1
-    #print('data X: ',X_new_file[0:3,0:5], np.shape(X_new_file), it)
+# Create a loss expression for training, i.e., a scalar objective we want
+# to minimize (for our multi-class problem, it is the cross-entropy loss):
+prediction = lasagne.layers.get_output(network)
+loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+loss = loss.mean()
 
-    if file_it == 1:
-        X_train = X_new_file
-    else:
-        X_train = np.vstack((X_train,X_new_file))
-    file_it+=1
-    print('shape of X_train: ', np.shape(X_train))
-
-############### Now write X_test to a csv file ###################
-'''
-with open('X_train.csv','wb') as csvfile:
-    result_writer = csv.writer(csvfile,delimiter=',')
-
-    X_shape = np.shape(X_train)         # 3400 x 260
-    for i in xrange(X_shape[0]):
-        for j in xrange(X_shape[1]) :
-            #result_writer.writerow([output[i],pred[i-1]])
-            result_writer.write([X_test[i,j]])
-'''
-np.savetxt('X_train.csv', X_train, delimiter=",")
+# Create update expressions for training, i.e., how to modify the
+# parameters at each training step. Here, we'll use Stochastic Gradient
+# Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+params = lasagne.layers.get_all_params(network, trainable=True)
+updates = lasagne.updates.nesterov_momentum(
+        loss, params, learning_rate=0.01, momentum=0.9)
 
 
 
-# images = gen_images(channelsLocation, np.random.rand(100, 30), 16, augment=True, pca=True, n_components=2)
+# Compile a function performing a training step on a mini-batch (by giving
+# the updates dictionary) and returning the corresponding training loss:
+train_fn = theano.function([input_var, target_var], loss, updates=updates)
+
+
+print("Starting training...")
+train_fn(X_train, y_train)
