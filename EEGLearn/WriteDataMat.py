01db498d3f13074f61c32df87a49e920dde0dab1
Andreas Wiedemann <wiedeman@atcremers46.informatik.tu-muenchen.de>
2016-07-05 21:22:12 +0200

    Add functionality of generating training averaged images

--- EEGLearn/WriteDataMat.py
+++ EEGLearn/WriteDataMat.py
@@ -4,9 +4,12 @@ import numpy as np
 import scipy.io as sio
 import csv
 import sys, getopt
+import eeg_cnn_lib
+import utils
 
-outformat = ""
-source = 'bci-challenge'
+import pdb
+
+
 
 
 def main(argv):
@@ -23,15 +26,36 @@ def main(argv):
             sys.exit()
         elif opt in ("-f", "--format"):
             outformat = arg
-            if outformat not in ["raw", "bands"]:
-                print '<format> = {raw | bands}'
+            if outformat not in ["raw", "avg_img"]:
+                print '<format> = {raw | avg_img}'
                 sys.exit()
                     
     print('Output format is ', outformat)
-        
+    
+    ############ Calculate position of electrodes ##############################################
+    # Radius and angle of the electrodes are given in the file ChannelsLocation.csv
+    # The library utils offer a function for conversion the polar coordinates into cartesian
+    # coordinates.
+    channelsLocation = np.zeros((56,2))
+    with open("../Other/ChannelsLocation.csv","r") as csvfile:
+        reader = csv.DictReader(csvfile,delimiter = ',')
+        rownum = 0
+        for row in reader:
+            # print(row['Radius'],row['Phi'])
+            # print(utils.pol2cart(float(row['Radius']),float(row['Phi'])))
+            channelsLocation[rownum] = utils.pol2cart(float(row['Radius']),float(row['Phi']))
+            rownum += 1
+    print("Channel locations loaded.")
     
     X_train=readX('../train/Data_*.csv', outformat)
     X_test =readX('../test/Data_*.csv', outformat)
+    pdb.set_trace()
+    features = np.vstack((X_train,X_test))
+    
+    features = eeg_cnn_lib.gen_images(channelsLocation, features, 16, augment=False, pca=False, n_components=2)
+    
+    X_train = features[:X_train.shape[0]]
+    X_test = features[X_train.shape[0]:]
     
     labels = np.loadtxt('../TrainLabels.csv', delimiter=',',skiprows=1,usecols=[1])  # Such that the labels matches X_train in order the list must be in alphabetical order
     
@@ -42,6 +66,7 @@ def main(argv):
 
 
 def readX(folder_path, outformat):
+    source = 'bci-challenge'
     print("Loading files of: ", folder_path)
     list_of_files = sorted(glob.glob(folder_path))
     X=[]
@@ -63,10 +88,10 @@ def readX(folder_path, outformat):
         for feedback_event in events:
             if outformat == "raw":
                 feature_vector = data[feedback_event : feedback_event+260, n_channel]
-            elif outformat == "bands":
+            elif outformat == "avg_img":
                 feature_matrix = data[feedback_event : feedback_event+260, :]
                 EEG = EEGrunt.EEGrunt("", "", source, "")
-                EEG.set_data(feature_matrix)
+                EEG.set_raw_data(feature_matrix)
                 # EEG.plot = 'show'
                 feature_vector = []
                 
@@ -87,7 +112,7 @@ def readX(folder_path, outformat):
                     beta = np.mean(EEG.bandpass(13,30))
                     # beta = EEG.bandpass(13,30)
                     feature_vector.append(beta)
-            
+                
             if X_new_file == []:
                 X_new_file = feature_vector
             else:
@@ -99,6 +124,11 @@ def readX(folder_path, outformat):
         else:
             X = np.vstack((X,X_new_file))
             print('shape of X_train: ', np.shape(X))
+    
+    
+    
+    
+    # X = gen_images(channelsLocation, X, 16, augment=False, pca=False, n_components=2)
     
     return X
 
